32a6c30688498f6c963d3ebc9f7e859b8f5705ee
Boris Bondarenko <borisoffficial@gmail.com>
2021-10-27 03:00:31 +0300

    first commit

---
+++ remote_run.sh
@@ -0,0 +1,35 @@
+# Run this script on remote server to prepare it to use
+
+if [ $# -eq 0 ]
+  then
+    echo "First arguments must be app name"
+    exit 1
+fi
+
+if [ -z $HOSTNAME ]
+  then
+    echo "It seems, you run script on your local machine"
+    exit 1
+fi
+
+# TODO Make only on new servers (add auto check)
+sudo apt-get update -y
+sudo apt-get upgrade -y
+
+# TODO Optimize (not install if exists)
+sudo apt install -y docker
+# TODO Fix docker-compose unsupported version problem
+sudo apt install -y docker-compose
+sudo apt install -y git
+# https://stackoverflow.com/questions/43671482/how-to-run-docker-compose-up-d-at-system-start-up
+sudo systemctl enable docker
+
+if ! cd $1; then
+  git clone $2
+  cd $1
+else
+  git pull
+fi
+
+docker-compose build
+docker-compose up -d
---
+++ upload.sh
@@ -0,0 +1,48 @@
+if [ $# -eq 0 ]
+  then
+    echo "🛑 First arguments must be server name!"
+    exit
+fi
+
+
+if [ ! -z "`git status | grep "not staged"`" ]; then
+   echo "🛑 You have not staged changes. Make 'git add .' "
+   exit
+fi
+
+if [ ! -z "`git status | grep "to be committed"`" ]; then
+   echo "🛑 You forgot to make 'git commit'"
+   exit
+fi
+
+if [ ! -z "`git status | grep "is ahead"`" ]; then
+   echo "🛑 You have not pushed changes. Make 'git push' "
+   exit
+fi
+
+echo "✅ Begin uploading upp to server: '$1'"
+
+REPO=`git remote -v | head -n 1 | awk '{print $2}'`
+APP_NAME=`echo $REPO | grep -oE "/[a-z-]+" - | cut -c2-`
+
+echo "Using repository '$REPO' and application '$APP_NAME'"
+
+if ssh $1 rm remote_run.sh; then
+    echo "✅ Script remote_run.sh deleted from server!"
+    else
+      echo "File remote_run.sh does not contained on server"
+  fi
+
+if ! scp ./production/remote_run.sh $1:~/remote_run.sh; then
+  echo "🛑 Copy remote_run.sh failed"
+  exit;
+  fi
+
+echo "✅ Script remote_run.sh copied!"
+
+if ! ssh $1 "./remote_run.sh $APP_NAME $REPO"; then
+  echo "🛑 Failed while running prepare.sh"
+  exit 1;
+fi
+
+echo "✅ Done! "
